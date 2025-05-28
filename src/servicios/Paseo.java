48b4debfac9dfe901612ab0ab3cbb4463daa3e9d
valenmendez01 <mendezvalentin824@gmail.com>
2025-05-28 20:47:48 -0300

    ccc

--- src/servicios/Paseo.java
+++ src/servicios/Paseo.java
@@ -1,4 +1,28 @@
 package servicios;
 
-public class Paseo {
+public class Paseo extends Servicio {
+    private boolean individual;
+
+    public Paseo(double precioBloque, String nombre, int cantidadBloques, boolean individual) {
+        super(precioBloque, nombre, cantidadBloques);
+        this.individual = individual;
+    }
+
+    public boolean getIndividual() {
+        return individual;
+    }
+
+    public void setIndividual(boolean individual) {
+        this.individual = individual;
+    }
+
+    @Override
+    public double calcularCosto(){
+        if (individual){
+            precioBloque =+ 1000;
+        } else {
+            precioBloque =+ 500;
+        }
+        return precioBloque * cantidadBloques;
+    };
 }
--- src/servicios/Servicio.java
+++ src/servicios/Servicio.java
@@ -1,4 +1,39 @@
 package servicios;
 
-public class Servicio {
+public abstract class Servicio {
+    protected String nombre;
+    protected double precioBloque;
+    protected int cantidadBloques;
+
+    public Servicio(double precioBloque, String nombre, int cantidadBloques) {
+        this.precioBloque = precioBloque;
+        this.nombre = nombre;
+        this.cantidadBloques = cantidadBloques;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precioBloque;
+    }
+
+    public void setPrecio(double precioBloque) {
+        this.precioBloque = precioBloque;
+    }
+
+    public int getCantidadBloques() {
+        return cantidadBloques * 30; // bloques de 30 minutos
+    }
+
+    public void setCantidadBloques(int cantidadBloques) {
+        this.cantidadBloques = cantidadBloques;
+    }
+
+    public abstract double calcularCosto();
 }
